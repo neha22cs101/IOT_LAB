c87526e1a107f3611d31f90dccfa0e60b9020ddf
neha22cs101 <neha.22cs101@sode-edu.in>
2025-08-11 15:45:51 +0530

    Add files via upload
    Backward sequence blinking of LED lights.

---
+++ L1c.ino
@@ -0,0 +1,15 @@
+void setup() {
+  for (int pin = 2; pin <= 6; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void loop() {
+  // Backward sequence
+  for (int pin = 6; pin >= 2; pin--) {
+    digitalWrite(pin, HIGH);
+    delay(1000);
+    digitalWrite(pin, LOW);
+    delay(1000);
+  }
+}
