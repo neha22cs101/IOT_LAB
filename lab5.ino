a856b43ba8043e4c039c67d4bb1a3eec375b1d32
neha22cs101 <neha.22cs101@sode-edu.in>
2025-09-08 15:50:39 +0530

    Add files via upload
    Keypad.

---
+++ lab5.ino
@@ -0,0 +1,25 @@
+#include<Keypad.h>
+const byte ROWS = 4;
+const byte COLS =3;
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {10,9,8,7};
+byte colPins[COLS] = {6,5,4};
+
+Keypad keypad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char key = keypad.getKey();
+  if(key){
+    Serial.println(key);
+  }
+}
